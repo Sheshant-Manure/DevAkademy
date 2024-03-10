71938a4a0aa27efec2db939394a1f28dabccc501
Sheshant-Manure <msheshant1997@gmail.com>
2024-03-10 16:24:56 +0530

    Update redirect url with env var

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 require('../config/passport-setup.js');
+require('dotenv').config();
 
 // This route will open a GitHub consent form where user will enter credentials and allow  access to their data on Github
 router.get('/github', passport.authenticate( 'github'));  
@@ -9,7 +10,7 @@ router.get('/github', passport.authenticate( 'github'));
 // If the user allows access to Github data, they are redirected to the following url along with query param
 // that contains a code which can be authenticated to get user data.
 router.get('/github/redirect', passport.authenticate('github'), (req, res) => {
-    res.redirect('http://localhost:3000/')
+    res.redirect(process.env.CLIENT_URL);
 })
 
 module.exports = router;
