1d6ee945fdd838bb234a86624452e3747dc54900
Sheshant-Manure <msheshant1997@gmail.com>
2024-03-10 16:17:21 +0530

    Update server url

--- client/src/config/serverURL.js
+++ client/src/config/serverURL.js
@@ -1,4 +1,4 @@
-const ServerURL = 'https://devakademy.onrender.com/' // Replace with 'https://devakademy.onrender.com/' when running production build and deploy. 
+const ServerURL = 'https://devakademy.onrender.com' // Replace with 'https://devakademy.onrender.com' when running production build and deploy. 
                                           // And for development purposes replace with 'http://localhost:8080'
 
 export default ServerURL;
