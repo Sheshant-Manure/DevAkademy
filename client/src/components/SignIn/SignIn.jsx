1e2f925153caf07e5bd4c3f2582201d2d146ad2b
Sheshant-Manure <msheshant1997@gmail.com>
2024-03-13 11:07:36 +0530

    Update homepage url

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -1,42 +1,37 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';  // Import Axios
 import style from './SignIn.module.css';
-import GitHubLogo from '../../assets/github.png'
+import GitHubLogo from '../../assets/github.png';
 import { setUser } from '../../redux/slices/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import ServerURL from '../../config/serverURL';
 
 const SignIn = () => {
   const dispatch = useDispatch();
-  const signedIn = useSelector((state)=>state.user.isAuthenticated);
-  useEffect(()=>{
+  const signedIn = useSelector((state) => state.user.isAuthenticated);
+
+  useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await fetch(`${ ServerURL }/userdata`, { credentials: 'include' });
-        const data = await response.json();
-        if(data._id) {
-          dispatch(setUser(data));
+        const response = await axios.get(`${ServerURL}/userdata`, { withCredentials: true });
+        if (response.data._id) {
+          dispatch(setUser(response.data));
         }
       } catch (error) {
         console.error('Error fetching user:', error);
       }
-    }
+    };
     fetchUser();
-  },[dispatch])
-  
-  return (
-      !signedIn ? (
-      <a className={style.SignInLink} href={`${ ServerURL }/auth/github`}>
-        <button className={style.gitHubButton}>
-          <img
-            className={style.gitHubLogo}
-            src={ GitHubLogo }
-            alt="GitHub Logo"
-          />
-          <b>Sign In With GitHub</b>
-        </button>
-      </a>
-     ): null
-  );
+  }, [dispatch]);
+
+  return !signedIn ? (
+    <a className={style.SignInLink} href={`${ServerURL}/auth/github`}>
+      <button className={style.gitHubButton}>
+        <img className={style.gitHubLogo} src={GitHubLogo} alt="GitHub Logo" />
+        <b>Sign In With GitHub</b>
+      </button>
+    </a>
+  ) : null;
 };
 
 export default SignIn;
