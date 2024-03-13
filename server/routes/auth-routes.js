5125846cc0e693d3747bcd00d6736239f706d0cd
Sheshant-Manure <msheshant1997@gmail.com>
2024-03-13 20:34:05 +0530

    cors testing

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const passport = require('passport');
 require('dotenv').config();
 
+const logOrigin = (req, res, next) => {
+    console.log(req.headers.host);
+    next();
+}
+
 // This route will open a GitHub consent form where user will enter credentials and allow  access to their data on Github
-router.get('/github', passport.authenticate( 'github'));  
+router.get('/github', logOrigin, passport.authenticate( 'github'));  
 
 // If the user allows access to Github data, they are redirected to the following url along with query param
 // that contains a code which can be authenticated to get user data.
-router.get('/github/redirect', passport.authenticate('github'), (req, res) => {
+router.get('/github/redirect', passport.authenticate('github'), logOrigin, (req, res) => {
     res.redirect(process.env.CLIENT_URL);
 })
 
